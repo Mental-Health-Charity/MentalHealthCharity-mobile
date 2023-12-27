149169478462151fafa7589ad7b5786d2b8af73b
Glakuta <glakuta@gmail.com>
2023-12-27 21:52:08 +0100

    Initial commit

---
+++ screens/Login.tsx
@@ -0,0 +1,14 @@
+import { Text, View, Button } from "react-native";
+import React from "react";
+
+type Props = {};
+
+const Login = (props: Props) => {
+  return (
+    <View>
+      <Text>Login</Text>
+    </View>
+  );
+};
+
+export default Login;
